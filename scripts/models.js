9d5f801b9c40f4a3f931403d1d92f5802b8d4853
Bobby Seidensticker <bseidensticker@gmail.com>
2013-06-08 17:45:30 -0700

    updates

--- scripts/models.js
+++ scripts/models.js
@@ -1,4 +1,4 @@
-namespace.module('bonanza.abstract', function (exports, require) {
+namespace.module('bonanza.models', function (exports, require) {
 
     exports.extend({
         Board: Board
@@ -7,16 +7,16 @@ namespace.module('bonanza.abstract', function (exports, require) {
     function Board(size, gemCount) {
         this.gems = [];
         this.gemCount = gemCount;
+        this.size = [size[0], size[1]];
         var i, j;
         for (i = 0; i < size[0]; i++) {
-            gems[i] = [];
+            this.gems[i] = [];
             for (j = 0; j < size[1]; j++) {
-                gems[i][j] = rand(0, gemCount);
+                this.gems[i][j] = rand(0, this.gemCount);
             }
         }
         for (var i = 0; i < size[1]; i++) { this.gems[i] = new Array(size[1]); }
 
-        
     }
 
     Board.prototype.checkMatches = function () {
@@ -30,6 +30,24 @@ namespace.module('bonanza.abstract', function (exports, require) {
         // if no matches, return undefined
         // if there are matches, return an array of matches, each match is a list of coordinates of poppable gems
     }
+
+    Board.prototype.adjacents = function (coord) {
+        var moves = [];
+        if (coord[0] > 0) {
+            moves[moves.length - 1] = [coord[0] - 1, coord[1]];
+        }
+        if (coord[0] < this.size[0] - 1) {
+            moves[moves.length - 1] = [coord[0] + 1, coord[1]];
+        }
+        if (coord[1] > 0) {
+            moves[moves.length - 1] = [coord[0], coord[1] - 1];
+        }
+        if (coord[1] < this.size[1] - 1) {
+            moves[moves.length - 1] = [coord[0], coord[1] + 1];
+        }
+        return moves;
+    }
+
 
     function rand(min, max) {
         // returns random integer i where: min <= i < max
